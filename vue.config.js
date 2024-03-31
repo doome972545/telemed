7131839a94cb5dd425d9602d94259ded5c091582
chayanon <chayanon.au@rmuti.ac.th>
2024-03-31 19:53:27 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   // devServer: {
-  //   disableHostCheck: true,
+  //   disableHostCheck: tru6e,
   // },
   publicPath: '/',
   lintOnSave: false,
